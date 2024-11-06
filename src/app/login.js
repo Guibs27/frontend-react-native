f1426b4e0e70abaa9825b34d0f6cd9bb5ec0a000
Guilherme Byz <gbizerradesouza@gmail.com>
2024-11-06 15:52:19 -0300

    add login & logout

--- src/app/login.js
+++ src/app/login.js
@@ -1,33 +1,87 @@
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
-import Footer from '../components/Footer';
-import Button from '../components/Button';
-import { useRouter } from 'expo-router';
+import { ScrollView, StyleSheet, View, Text, TextInput, Alert } from 'react-native'
+import Button from '../components/Button'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
+import { useLoginStore } from '../stores/useLoginStore'
 
 export default function Login() {
-  const router = useRouter();
+  const [txtEmail, setTxtEmail] = useState('')
+  const [txtPass, setTxtPass] = useState('')
+  const { login: loginStore } = useLoginStore()
+  const router = useRouter()
+
+  const handleLogin = async () => {
+    const login = {
+      email: txtEmail,
+      pass: txtPass
+    }
+
+    const response = await fetch('http://localhost:3000/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(login)
+    })
+
+    if (response.ok) {
+      const data = await response.json()
+      console.log(data)
+      loginStore({ accessToken: data?.accessToken, ...data.user })
+      router.push('/home')
+    } else {
+      const data = await response.json()
+      Alert.alert('Erro ao logar.')
+      console.log(data?.error)
+    }
+    return
+  }
+
   return (
-    <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.content}>
-        <View style={styles.mainContent}>
-          <Text style={{ fontSize: 30 }}>Login</Text>
-          <Button onPress={() => router.push('/signup')}>Cadastre-se</Button>
-        </View>
-      </ScrollView>
-      <Footer />
-    </View>
-  );
+    <ScrollView style={styles.container}>
+      <View style={{ flex: 1, marginTop: 100, justifyContent: 'center', alignItems: 'center' }}>
+
+        <Text>Email:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setTxtEmail}
+          value={txtEmail}
+        />
+        <Text style={{ marginTop: 10 }}>Senha:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setTxtPass}
+          value={txtPass}
+          secureTextEntry={true}
+        />
+        <Button onPress={handleLogin}>Entrar</Button>
+
+        <View style={styles.divisor} />
+
+        <Button onPress={() => router.push('/signup')}>Cadastre-se</Button>
+      </View>
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
-  content: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    paddingBottom: 250
+  input: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#e3e3e3',
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    marginVertical: 5,
+    borderRadius: 8,
+    borderColor: '#8a8a8a'
   },
-  mainContent: {
-    alignItems: 'center'
-  },
-});
+  divisor: {
+    borderBottomColor: '#CCC',
+    borderBottomWidth: 1,
+    width: '90%',
+    marginVertical: 50,
+  }
+})
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,24 @@
 import { View, Text, StyleSheet } from 'react-native'
 import { Image } from 'expo-image'
-import Feather from '@expo/vector-icons/Feather';
 import { Link } from 'expo-router'
+import { useLoginStore } from '../stores/useLoginStore'
+import Feather from '@expo/vector-icons/Feather'
 
 export default function Header() {
+  const { name, avatar } = useLoginStore()
+
   return (
     <View style={styles.header}>
       <View style={styles.user}>
         <Image
           style={styles.avatar}
-          source="https://avatars.githubusercontent.com/u/145155960?s=400&u=5c9ece5fe9cabb80b24e29009a8daa627b41e76e&v=4"
-        // source={require('caminho_arquivo')}
+          source={avatar} // Fonte local
+        // source="https://avatars.githubusercontent.com/u/145155960?s=400&u=5c9ece5fe9cabb80b24e29009a8daa627b41e76e&v=4"
         />
-        <Text style={styles.name}>Guilherme Byz</Text>
+        <Text style={styles.name}>{name}</Text>
       </View>
       <Link href="signup">
-        <Feather style={styles.menu} name="plus" size={28} color="white" />
+        <Feather style={styles.menu} name="plus" size={24} color="black" />
       </Link>
     </View>
   )
